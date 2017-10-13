f17733645891e39dbed8079c8c9e6bd5409ac7d1
Matthew McLeod <matthew.mcleod@mavs.uta.edu>
2017-10-13 16:11:51 -0500

    Filling in Hello World example

--- PDXAlexaSkill/index.js
+++ PDXAlexaSkill/index.js
@@ -1,1 +1,8 @@
-//TODO: everything
+import Alexa from 'alexa-sdk'
+import handlers from 'handlers'
+
+exports.handler = (event, context, callback) => {
+    const alexa = Alexa.handler(event, context, callback);
+    alexa.registerHandlers(handlers);
+    alexa.execute();
+};
